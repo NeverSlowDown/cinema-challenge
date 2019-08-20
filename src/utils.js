710525538d370c5b5c640c4aeb5b5c3b6d2409de
tomi <tomel95@hotmail.com>
2019-08-20 01:39:59 -0300

    adding something

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
 export const getStars = vote => {
-  if(vote >= 0 && vote <= 2){return 1}
-  if(vote > 2 && vote <= 4){return 2}
-  if(vote > 4 && vote <= 6){return 3}
-  if(vote > 6 && vote <= 8){return 4}
-  if(vote > 8 && vote <= 10){return 5}
+  let result = 1;
+  if(vote >= 0 && vote <= 2){result = 1}
+  if(vote > 2 && vote <= 4){result = 2}
+  if(vote > 4 && vote <= 6){result = 3}
+  if(vote > 6 && vote <= 8){result = 4}
+  if(vote > 8 && vote <= 10){result = 5}
+  
+  return result;
 }
